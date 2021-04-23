458d7c5df7584c9457935c1065e4e3c83cf22db4
chethanbr86 <chethanbr86@gmail.com>
2021-04-23 23:14:31 +0530

    Create flask_dynamic.py

---
+++ flask_myfile/flask_dynamic.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1> Hello Puppy! </h1>"
+
+#2nd example
+@app.route('/information')
+def info():
+    return "<h1> Hello Puppy! you are cute!! </h1>"
+
+@app.route('/puppy/<name>')
+def puppypy(name):
+    return f"<h1> This is a page for {name.upper()} </h1>"
+
+if __name__ == '__main__':
+    app.run()

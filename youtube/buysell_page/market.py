27fcc728908e1e1aa065792ae9d71d8fbefd8c67
chethanbr86 <chethanbr86@gmail.com>
2021-05-01 21:07:05 +0530

    Update market.py

--- youtube/buysell_page/market.py
+++ youtube/buysell_page/market.py
@@ -1,16 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def hello_world():
+#     # return 'Hello World'
+#     return '<h1>Hello World</h1>'
+
+# @app.route('/about')
+# def about_page1():
+#     return '<h1>This page is about </h1>'
+
+# @app.route('/about/<username>')
+# def about_page2(username):
+#     return f'<h1>This page is about {username}!</h1>'
+
 @app.route('/')
-def hello_world():
-    # return 'Hello World'
-    return '<h1>Hello World</h1>'
-
-@app.route('/about')
-def about_page1():
-    return '<h1>This page is about </h1>'
-
-@app.route('/about/<username>')
-def about_page2(username):
-    return f'<h1>This page is about {username}!</h1>'
+def home_page():
+    return render_template('home.html')

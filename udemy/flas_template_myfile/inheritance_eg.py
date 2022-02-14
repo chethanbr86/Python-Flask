2a36d97842bf42cdca2c41c117e04bb1820a6cf4
chethanbr86 <chethanbr86@gmail.com>
2022-02-14 17:37:44 +0530

    updates

--- udemy/flas_template_myfile/inheritance_eg.py
+++ udemy/flas_template_myfile/inheritance_eg.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():        
-    return render_template('home.html')
+    return render_template('home_inherit.html')
 
 @app.route('/puppy/<name>')
 def pup_name(name):
-    return render_template('puppy.html',name=name)
+    return render_template('puppy_inherit.html',name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)

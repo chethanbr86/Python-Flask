f482e7dc518197265a234e3e91207888ad505b4a
chethanbr86 <chethanbr86@gmail.com>
2022-02-16 09:10:40 +0530

    updates

--- udemy/db_ps4/games.py
+++ udemy/db_ps4/games.py
@@ -10,7 +10,7 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 
 #SQL section
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'games_data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)

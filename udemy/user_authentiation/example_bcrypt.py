780ae499aa3948d8054eadca2ceb420361b7f531
chethanbr86 <chethanbr86@gmail.com>
2022-02-16 21:32:14 +0530

    updates

--- udemy/user_authentiation/example_bcrypt.py
+++ udemy/user_authentiation/example_bcrypt.py
@@ -2,8 +2,10 @@ from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
 password = 'supersecretpassword'
+print(password)
+
 hashed_password = bcrypt.generate_password_hash(password=password)
 print(hashed_password)
 
-check = bcrypt.check_password_hash(hashed_password,'wrongpassword') #supersecretpassword
+check = bcrypt.check_password_hash(hashed_password,'supersecretpassword') #wrongpassword put this to get False
 print(check)
--- udemy/user_authentiation/example_werkzeug.py
+++ udemy/user_authentiation/example_werkzeug.py
@@ -3,7 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 hashed_pass = generate_password_hash('mypassword')
 print(hashed_pass)
 
-check = check_password_hash(hashed_pass, 'mypassword') #otherpassword
+check = check_password_hash(hashed_pass, 'otherpassword') #'mypassword'
 print(check)
 
 #you can directly work like this instead of calling as in bcrypt, this way also works in bcrypt

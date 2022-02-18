af7111dff779fcdff17e3edbed2b4517fee12426
chethanbr86 <chethanbr86@gmail.com>
2022-02-18 13:59:35 +0530

    updates

--- udemy/db_view/forms_site.py
+++ udemy/db_view/forms_site.py
@@ -3,6 +3,7 @@ from wtforms import StringField, IntegerField, SubmitField
 
 class AddForm(FlaskForm):
     name = StringField('Name of puppy: ')
+    # year = RadioField or BooleanField or SelectField - try these
     submit = SubmitField('Add puppy')
 
 class DelForm(FlaskForm):

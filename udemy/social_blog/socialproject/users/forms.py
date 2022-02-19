55ddfffaac1cab9eee0e866a93790812f5b987c5
chethanbr86 <chethanbr86@gmail.com>
2022-02-19 12:19:34 +0530

    update

---
+++ udemy/social_blog/socialproject/users/forms.py
@@ -0,0 +1,36 @@
+from ast import Pass
+from email import message
+from wsgiref.validate import validator
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
+from flask_wtf.file import FileField, FileAllowed
+
+from flask_login import current_user
+from socialproject.models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    pasword = PasswordField('Password',validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    username = StringField('Username',validators=[DataRequired()])
+    pasword = PasswordField('Password',validators=[DataRequired(), EqualTo('pass_confirm'), message='Passwords must match!'])
+    pass_confirm = PasswordField('Confirm Password'validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Your email has been registered already!')
+
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Your username has been registered already!')
+
+class UpdateUserForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    pasword = PasswordField('Password',validators=[DataRequired()])
+    pitcure = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    submit = submit = SubmitField('Update')

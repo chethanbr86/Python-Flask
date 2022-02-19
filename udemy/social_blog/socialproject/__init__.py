836113480ae5bdb734da1299dad4a031d165f0ec
chethanbr86 <chethanbr86@gmail.com>
2022-02-19 11:09:39 +0530

    new updates

--- udemy/social_blog/socialproject/__init__.py
+++ udemy/social_blog/socialproject/__init__.py
@@ -1,3 +1,8 @@
 from flask import Flask
+from socialproject.core.views import core
+from socialproject.error_pages.handlers import error_pages
 
 app = Flask(__name__)
+
+app.register_blueprint(core)
+app.register_blueprint(error_pages)

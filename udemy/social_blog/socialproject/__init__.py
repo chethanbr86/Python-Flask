e6eda2a35b8ebae9d07680e67f622979001db88f
chethanbr86 <chethanbr86@gmail.com>
2022-02-19 11:56:09 +0530

    new update

--- udemy/social_blog/socialproject/__init__.py
+++ udemy/social_blog/socialproject/__init__.py
@@ -1,8 +1,27 @@
+import os
 from flask import Flask
-from socialproject.core.views import core
-from socialproject.error_pages.handlers import error_pages
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+#Database
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'gamedata.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+#Login
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+from socialproject.core.views import core
+from socialproject.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
 app.register_blueprint(error_pages)

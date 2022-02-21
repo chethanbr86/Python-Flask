f7a112304cc97082736cc8cd374e50a55e112679
chethanbr86 <chethanbr86@gmail.com>
2022-02-21 18:12:47 +0530

    updated

--- udemy/zero_to_mastery/selenium_basics/automation.py
+++ udemy/zero_to_mastery/selenium_basics/automation.py
@@ -1,5 +1,11 @@
+from email.policy import default
 from selenium import webdriver
 
 chrome_browser = webdriver.Chrome('./chromedriver')
-print(chrome_browser)
-
+chrome_browser.maximize_window()
+# http://allselenium.info/python-selenium-commands-cheat-sheet-frequently-used/ - cheetsheet
+chrome_browser.get("https://demo.seleniumeasy.com/basic-first-form-demo.html")
+assert 'Selenium Easy Demo - Simple Form to Automate using Selenium' in chrome_browser.title
+button_text = chrome_browser.find_element_by_class_name('btn-default')
+print(button_text.get_attribute('innerHTML'))
+assert 'Show Message' in chrome_browser.page_source

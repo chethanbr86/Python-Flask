9df8ce6e0898af3b0073f02789d5fbf93282bb0d
chethanbr86 <chethanbr86@gmail.com>
2022-02-23 14:29:26 +0530

    updates

--- udemy/python-flask-bootcamp/social_blog/socialproject/users/forms.py
+++ udemy/python-flask-bootcamp/social_blog/socialproject/users/forms.py
@@ -1,5 +1,3 @@
-from email import message
-from wsgiref.validate import validator
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Email, EqualTo
@@ -15,8 +13,8 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
-    username = StringField('Username',validators=[DataRequired()])
-    password = PasswordField('Password',validators=[DataRequired(), EqualTo('pass_confirm')]) 
+    username = StringField('UserName',validators=[DataRequired()])
+    password = PasswordField('Password',validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords must match')]) 
     pass_confirm = PasswordField('Confirm Password',validators=[DataRequired()])
     submit = SubmitField('Register')
 
@@ -29,9 +27,8 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Your username has been registered already!')
 
 class UpdateUserForm(FlaskForm):
-    username = StringField('Username',validators=[DataRequired()])
     email = StringField('Email',validators=[DataRequired(),Email()])
-    password = PasswordField('Password',validators=[DataRequired()])
+    username = StringField('Username',validators=[DataRequired()])        
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png','jpeg'])])
     submit = SubmitField('Update')
 

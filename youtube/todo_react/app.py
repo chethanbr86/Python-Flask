c5b806ae6c803b1e0d201a9cbe9f13040937baf2
chethanbr86 <chethanbr86@gmail.com>
2022-11-20 21:22:01 +0530

    todo in progress

--- youtube/todo_react/app.py
+++ youtube/todo_react/app.py
@@ -1,46 +1,55 @@
 from flask import Flask, render_template, request, redirect, url_for
+from form import Todo
+#https://www.youtube.com/watch?v=cb1vy1HpVwk&t=1586s
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'password'
 
+#direct return
 # @app.route('/')
 # def index():
 #     return f'Hello stranger !'
 
+#html return
 # @app.route('/')
 # def index_html():
 #     return render_template('hello.html')
 
+#direct name string
 # @app.route('/<name>') #('/<string:name>') or ('/<int:name>')
 # def greet_stranger(name):
 #     return f'Hello {name}!'
 
+#html name string
 # @app.route('/<name>') #dynamic single name here and in html
 # def index_html(name):
 #     return render_template('hello.html', name = name)
 
-
+#without methods
 # @app.route('/request_method')
 # def hello_world():
 #     request_method = request.method
 #     return render_template('hello_request.html', request_method=request_method)
 
+# #Same as above with methods
+# @app.route('/', methods=['GET','POST'])
+# def hello_world():
+#     request_method = request.method
 
-#Same as above with methods
-@app.route('/', methods=['GET','POST'])
-def hello_world():
-    request_method = request.method
+#     if request.method == 'POST':
+#         first_name = request.form['first_name']
+#         return redirect(url_for('name', first_name=first_name))
+#     return render_template('hello_request.html', request_method=request_method)
 
-    if request.method == 'POST':
-        print('-----------------------')
-        print(request.form)
-        print('-----------------------')
-        return redirect(url_for('name'))
-    return render_template('hello_request.html', request_method=request_method)
-#See how get changes to post as you submit - not working or me
+# #routing the above
+# @app.route('/name/<first_name>') #('/<string:name>') or ('/<int:name>')
+# def name(first_name):
+#     return f'{first_name}'
 
-@app.route('/name') #('/<string:name>') or ('/<int:name>')
-def name():
-    return 'name'
+@app.route('/todo', methods=['GET'])
+def todo():
+    todo_form = Todo()
+    return render_template('todo.html', form=todo_form)
 
 if __name__ == '__main__':
     app.run(debug=True)

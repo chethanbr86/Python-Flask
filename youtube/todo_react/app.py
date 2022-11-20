0093bb3fa3b69bb76e049ba1e825611269fb8717
chethanbr86 <chethanbr86@gmail.com>
2022-11-20 14:58:20 +0530

    small changes

--- youtube/todo_react/app.py
+++ youtube/todo_react/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return f'Hello stranger !'
+# @app.route('/')
+# def index():
+#     return f'Hello stranger !'
 
 # @app.route('/')
 # def index_html():
@@ -14,9 +14,9 @@ def index():
 # def greet_stranger(name):
 #     return f'Hello {name}!'
 
-@app.route('/<name>') #dynamic single name here and in html
-def index_html(name):
-    return render_template('hello.html', name = name)
+# @app.route('/<name>') #dynamic single name here and in html
+# def index_html(name):
+#     return render_template('hello.html', name = name)
 
 
 # @app.route('/request_method')
@@ -26,11 +26,21 @@ def index_html(name):
 
 
 #Same as above with methods
-@app.route('/request_method', methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def hello_world():
     request_method = request.method
+
+    if request.method == 'POST':
+        print('-----------------------')
+        print(request.form)
+        print('-----------------------')
+        return redirect(url_for('name'))
     return render_template('hello_request.html', request_method=request_method)
-    #See how get changes to post as you submit - not working or me
+#See how get changes to post as you submit - not working or me
+
+@app.route('/name') #('/<string:name>') or ('/<int:name>')
+def name():
+    return 'name'
 
 if __name__ == '__main__':
     app.run(debug=True)

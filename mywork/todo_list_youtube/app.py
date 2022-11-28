136d44fe0af1434526792e75b26e7da4ad929e17
chethanbr86 <chethanbr86@gmail.com>
2022-11-28 15:05:37 +0530

    some changes

--- mywork/todo_list_youtube/app.py
+++ mywork/todo_list_youtube/app.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+# app.config['SECRET_KEY'] = 'mysecretkey'
+# basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todolist.db' #'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+class Task_list(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(length=20), nullable=False, unique=True)
+    date = db.Column(db.datetime(), nullable=False)
+    status = db.Column(db.String(length=10), nullable=False)
+    summary = db.Column(db.String(100))
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
+#without database
 @app.route('/todo')
 def todo():
     tasks = [

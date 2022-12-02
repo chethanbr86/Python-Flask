ba6af232711b097a3830a150a77ce07546917a26
chethanbr86 <chethanbr86@gmail.com>
2022-12-02 18:18:49 +0530

    working for now

--- mywork/todo_list_youtube/app.py
+++ mywork/todo_list_youtube/app.py
@@ -1,48 +1,4 @@
-# from task_model import app
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'mysecretkey'
-# basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tododata.db'  #'sqlite:///'+os.path.join(basedir, 'todolist.sqlite') 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-from datetime import datetime
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(length=15), nullable=False, unique=True)
-#     email_address = db.Column(db.String(length=20), nullable=False, unique=True)
-#     password_hash = db.Column(db.String(length=60), nullable=False)
-#     hours = db.Column(db.Integer(), nullable=False, default=24)
-
-class Task_list(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(length=20), nullable=False, unique=True)
-    date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    status = db.Column(db.String(length=10), nullable=False)
-    summary = db.Column(db.String(100))
-
-    def __repr__(self):
-        return f'Task {self.id, self.name, s}'
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-@app.route('/todo')
-def todo():
-    # tasks = [
-    #     {'id':1, 'name':'task1',  'status': 1}, #'date': datetime.now(),
-    #     {'id':2, 'name':'task2',  'status': 2},
-    #     {'id':3, 'name':'task3',  'status': 3}
-    # ]
-    # return render_template('todo.html', tasks=tasks)
-    tasks = Task_list.query.all()
-    return render_template('todo.html', tasks=tasks)
+from task_model import app
 
 if __name__ == '__main__':
     app.run(debug=True)
--- mywork/todo_list_youtube/task_model/models.py
+++ mywork/todo_list_youtube/task_model/models.py
@@ -1,19 +1,24 @@
 from task_model import db
 from datetime import datetime
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(length=15), nullable=False, unique=True)
-#     email_address = db.Column(db.String(length=20), nullable=False, unique=True)
-#     password_hash = db.Column(db.String(length=60), nullable=False)
-#     hours = db.Column(db.Integer(), nullable=False, default=24)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(length=15), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=20), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    hours = db.Column(db.Integer(), nullable=False, default=24)
+    tasks = db.relationship('Task_list', backref='owned_user', lazy=True)
+
+    def __repr__(self):
+        return f'User {self.id, self.username, self.email_address}'
 
 class Task_list(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(length=20), nullable=False, unique=True)
-    # date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     status = db.Column(db.String(length=10), nullable=False)
     summary = db.Column(db.String(100))
+    owner = db.Column(db.Integer(), db.ForeignKey(User.username))
 
     def __repr__(self):
-        return f'Task {self.id, self.name}'
+        return f'Task {self.id, self.name, self.date}'

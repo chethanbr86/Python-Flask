b66154241550066f593004b484e0b95e11e97944
chethanbr86 <chethanbr86@gmail.com>
2022-12-03 12:13:10 +0530

    sumbit not working

--- mywork/todo_list_youtube/task_model/forms.py
+++ mywork/todo_list_youtube/task_model/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField
 
 class RegistrationForm(FlaskForm):
     username = StringField(label='User Name: ')
     email_address = StringField(label='Email Address: ')
     password1 = PasswordField(label='Password: ')
     password2 = PasswordField(label='Confirm Password: ')
-    sumbit = SubmitField(label='Create Account')
+    submit = SubmitField(label='Create Account')
 
 
--- mywork/todo_list_youtube/task_model/views.py
+++ mywork/todo_list_youtube/task_model/views.py
@@ -1,7 +1,6 @@
-from flask import render_template
-from task_model import app
-from task_model.models import Task_list
-from datetime import datetime
+from flask import render_template, redirect, url_for
+from task_model import app, db
+from task_model.models import Task_list, User
 from task_model.forms import RegistrationForm
 
 @app.route('/')
@@ -20,7 +19,12 @@ def todo():
     tasks = Task_list.query.all()
     return render_template('todo.html', tasks=tasks)
 
-@app.route('/register_page')
+@app.route('/register_page', methods=['GET','POST'])
 def register_page():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data, email_address=form.email_address.data, password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('todo'))
     return render_template('register.html', form=form)

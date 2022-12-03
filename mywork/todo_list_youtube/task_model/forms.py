9b4536c50d80baad56eb2f82f6840ac6169ee797
chethanbr86 <chethanbr86@gmail.com>
2022-12-03 09:52:52 +0530

    Works well until now

--- mywork/todo_list_youtube/task_model/forms.py
+++ mywork/todo_list_youtube/task_model/forms.py
@@ -2,10 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField, PasswordField
 
 class RegistrationForm(FlaskForm):
-    username = StringField(label='username')
-    email_address = StringField(label='email')
-    password1 = PasswordField(label='password1')
-    password2 = PasswordField(label='password2')
-    sumbit = SubmitField(label='submit')
+    username = StringField(label='User Name: ')
+    email_address = StringField(label='Email Address: ')
+    password1 = PasswordField(label='Password: ')
+    password2 = PasswordField(label='Confirm Password: ')
+    sumbit = SubmitField(label='Create Account')
 
 

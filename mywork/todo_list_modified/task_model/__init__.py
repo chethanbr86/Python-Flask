871e4e8680bc801eef0e9ddd26935eb3db735115
chethanbr86 <chethanbr86@gmail.com>
2022-12-11 12:21:45 +0530

    hmm

--- mywork/todo_list_modified/task_model/__init__.py
+++ mywork/todo_list_modified/task_model/__init__.py
@@ -1,5 +1,16 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(32).hex()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# from task_model.todo_viewform.views import 
+# from task_model.registration.views import 
+
+# app.register_blueprint()
+# app.register_blueprint()
 

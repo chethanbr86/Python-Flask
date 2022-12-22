722f65ae2879541318a4ea5572de28b11cb43ff8
chethanbr86 <chethanbr86@gmail.com>
2022-12-22 21:02:53 +0530

    todo_final

--- mywork/todo_list_final/task_model/__init__.py
+++ mywork/todo_list_final/task_model/__init__.py
@@ -8,9 +8,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todolist.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# from task_model.todo_viewform.views import 
+from task_model.todo_viewform.task_views import todo_blueprint
 # from task_model.registration.views import 
 
-# app.register_blueprint()
+app.register_blueprint(todo_blueprint)
 # app.register_blueprint()
 
--- mywork/todo_list_final/task_model/todo_viewform/task_views.py
+++ mywork/todo_list_final/task_model/todo_viewform/task_views.py
@@ -3,7 +3,7 @@ from task_model import db
 from task_model.models import Todo_List
 from task_model.todo_viewform.task_form import AddForm, EditForm, DelForm
 
-todo_blueprint = Blueprint('todo_blueprint', __name__)
+todo_blueprint = Blueprint('todo_viewform', __name__)
 
 @todo_blueprint.route('/')
 def home():

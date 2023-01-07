6192355542667f7e9b0c188ec016272357f0ad30
chethanbr86 <chethanbr86@gmail.com>
2023-01-07 18:02:06 +0530

    almost complete

--- mywork/todo_list_final/task_model/models.py
+++ mywork/todo_list_final/task_model/models.py
@@ -6,9 +6,9 @@ from datetime import datetime
 class Todo_List(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(length=30), nullable=False, unique=True)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    task_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     task_status = db.Column(db.String(length=10), nullable=False)
-    summary = db.Column(db.Text(100))
+    task_summary = db.Column(db.Text(100))
 
-    def __repr__(self):
-        return f'Task = {self.id, self.name, self.date, self.status}'    
+    # def __repr__(self):
+    #     return f'Task = {self.id, self.name, self.date, self.status}'    

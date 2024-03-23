5056ab794eadc810f7f129616619a6802c2ff3fb
chethanbr86 <chethanbr86@gmail.com>
2024-03-23 20:46:52 +0530

    v2

--- Banking_logic.py
+++ Banking_logic.py
@@ -1,6 +1,7 @@
+income_balance = 0
+def income_amt(income_balance):
+    input_amt = int(input('Adding to account Rs.: '))    
+    income_balance = income_balance + input_amt
+    return income_balance
 
-input_amt = int(input('Adding to account Rs.: '))
-balance = []
-balance = balance.append(input_amt)
-print(balance)
-
+print(income_amt(income_balance))

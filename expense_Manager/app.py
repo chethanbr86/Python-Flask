8ded952c651dea4e54130d75af1df52ffc8fbd4b
chethanbr86 <chethanbr86@gmail.com>
2024-12-25 12:24:13 +0530

    pf v2 - not imp, take pf v1

--- expense_Manager/app.py
+++ expense_Manager/app.py
@@ -1,44 +1,48 @@
 class Account:
-    balance = 0
-    income_bank = [] #convert to dict along with category
-    expense_bank = [] #convert to dict along with category
+    income_bank = [] 
+    expense_bank = [] 
     bank_dict = {}
+    hbalance,ibalance,pbalance = 0,0,0
+    total_balance = 0
 
     @classmethod
     def hbank(cls,income,expense,category):
-        cls.balance += income #or Account.balance works
+        cls.hbalance += income  #hbalance cannot be used here, should i initiate it as class variable?
         cls.income_bank.append(income)
-        cls.balance -= expense   
+        cls.hbalance -= expense   
         cls.expense_bank.append(expense)
-        cls.bank_dict[category] = {'income': income, 'expense': expense} #instead of income or expense as key, give category as key with if income or expense
-        return cls.balance
+        cls.bank_dict[category] = {'income': income, 'expense': expense} 
+        cls.total_balance = cls.hbalance
+        return f'hbalance: {cls.hbalance}'
 
     @classmethod
     def ibank(cls,income,expense,category):
-        cls.balance += income #or Account.balance works
+        cls.ibalance += income 
         cls.income_bank.append(income)
-        cls.balance -= expense   
+        cls.ibalance -= expense   
         cls.expense_bank.append(expense)
         cls.bank_dict[category] = {'income': income, 'expense': expense}
-        return cls.balance
+        cls.total_balance = cls.ibalance
+        return f'ibalance: {cls.ibalance}'
 
     @classmethod
     def pbank(cls,income,expense,category):
-        cls.balance += income #or Account.balance works
+        cls.pbalance += income 
         cls.income_bank.append(income)
-        cls.balance -= expense   
+        cls.pbalance -= expense   
         cls.expense_bank.append(expense)
         cls.bank_dict[category] = {'income': income, 'expense': expense}
-        return cls.balance
+        cls.total_balance = cls.pbalance
+        return f'pbalance: {cls.pbalance}'
 
     def __str__(cls):
-        return f'Account balance: {cls.balance}, income: {cls.income_bank}, expense: {cls.expense_bank}, bank_dict: {cls.bank_dict}'
-        #try to return with for along with enumerate
+        return f'Total Balance: {cls.total_balance}\n income: {cls.income_bank}\n expense: {cls.expense_bank}\n bank_dict: {cls.bank_dict}'
+        #try to return with 'for' along with enumerate
 
-print(Account.hbank(1000,10,'sal1'))
-print(Account.ibank(1000,20,'exp1'))
-print(Account.pbank(1000,30,'sav1'))
-print(Account.hbank(2000,20,'sal2'))
-print(Account.ibank(3000,30,'exp2'))
-print(Account.pbank(4500,40,'sav2'))
+print(Account.hbank(10,2,'sal1'))
+print(Account.ibank(20,4,'exp1'))
+print(Account.pbank(30,6,'sav1'))
+print(Account.hbank(15,1,'sal2'))
+print(Account.ibank(30,3,'exp2'))
+print(Account.pbank(45,5,'sav2'))
 print(Account())

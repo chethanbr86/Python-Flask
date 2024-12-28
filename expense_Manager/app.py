4e5fe6c4384011f9c03956b02543c2a5e0635950
Chethan BR <chethanbr86@gmail.com>
2024-12-28 19:59:45 +0530

    pf v9

--- expense_Manager/app.py
+++ expense_Manager/app.py
@@ -1,7 +1,10 @@
 class Account:
     hbank_balance = 0
+    hbank_list = []
     ibank_balance = 0
+    ibank_list = []
     pbank_balance = 0
+    pbank_list = []
     total_balance = 0
     category_list = []
 
@@ -13,29 +16,27 @@ class Account:
         if bank == 'hbank':
             if stat == 'income':
                 cls.hbank_balance += amount
-                cls.category_list.append(category)
             else:
                 cls.hbank_balance -= amount
-                cls.category_list.append(category)
+            cls.hbank_list.append(cls.hbank_balance)
+            cls.category_list.append(category)
             return cls.hbank_balance
         elif bank == 'ibank':
             if stat == 'income':
                 cls.ibank_balance += amount
-                cls.category_list.append(category)
             else:
                 cls.ibank_balance -= amount
-                cls.category_list.append(category)
+            cls.ibank_list.append(cls.ibank_balance)
+            cls.category_list.append(category)
             return cls.ibank_balance
         elif bank == 'pbank':
             if stat == 'income':
                 cls.pbank_balance += amount
-                cls.category_list.append(category)
             else:
                 cls.pbank_balance -= amount
-                cls.category_list.append(category)
+            cls.pbank_list.append(cls.pbank_balance)
+            cls.category_list.append(category)
             return cls.pbank_balance
-        else:
-            raise ValueError("Invalid bank name")
     
     @classmethod
     def total_Balance(cls):
@@ -43,7 +44,7 @@ class Account:
         return cls.total_balance
 
     def __str__(cls):
-        return f'Total balance: {Account.total_Balance()}, hbank: {cls.hbank_balance}, ibank: {cls.ibank_balance}, pbank: {cls.pbank_balance}, category: {cls.category_list}'
+        return f'Total balance: {Account.total_Balance()}\n hbank: {cls.hbank_list}\n ibank: {cls.ibank_list}\n pbank: {cls.pbank_list}\n category: {cls.category_list}'
         
 print(Account.update_balance('hbank',1000,'income','sal'))
 print(Account.update_balance('ibank',1000,'income','transfer'))
@@ -52,13 +53,3 @@ print(Account.update_balance('hbank',200,'expense','game'))
 print(Account.update_balance('ibank',300,'expense','auto'))
 print(Account.update_balance('pbank',400,'expense','gold'))
 print(Account())
-
-# while True:
-#     transact = input('Enter if the transaction is for hbank or ibank or pbank by inputting 1 or 2 or 3? ')
-#     if transact == 1:
-#         bank = 'hbank'
-#     elif transact == 2:
-#         bank = 'ibank'
-#     else:
-#         bank = 'pbank'
-#         #incomplete

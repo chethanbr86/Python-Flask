bbd3b806e65cc0cc18094c5be17226ccffc68e82
Chethan BR <chethanbr86@gmail.com>
2024-12-28 20:21:10 +0530

    pf v10

--- expense_Manager/app.py
+++ expense_Manager/app.py
@@ -1,41 +1,37 @@
 class Account:
     hbank_balance = 0
-    hbank_list = []
+    hbank_list = {}
     ibank_balance = 0
-    ibank_list = []
+    ibank_list = {}
     pbank_balance = 0
-    pbank_list = []
+    pbank_list = {}
     total_balance = 0
-    category_list = []
 
-#Here all the variables are called within single function, we can make 2 classes (inheritance) one for Account (just bank) and other class for 3 functions amount, category, date within class
-#And also take input within the function rather than outside
-#https://youtu.be/Dn1EjhcQk64?si=2mxecIq8Twdu37FB - follow for the output type without excel
     @classmethod
     def update_balance(cls, bank, amount, stat, category):
         if bank == 'hbank':
             if stat == 'income':
                 cls.hbank_balance += amount
+                cls.hbank_list[stat] = {category:cls.hbank_balance}
             else:
                 cls.hbank_balance -= amount
-            cls.hbank_list.append(cls.hbank_balance)
-            cls.category_list.append(category)
+                cls.hbank_list[stat] = {category:cls.hbank_balance}
             return cls.hbank_balance
         elif bank == 'ibank':
             if stat == 'income':
                 cls.ibank_balance += amount
+                cls.ibank_list[stat] = {category:cls.ibank_balance}
             else:
                 cls.ibank_balance -= amount
-            cls.ibank_list.append(cls.ibank_balance)
-            cls.category_list.append(category)
+                cls.ibank_list[stat] = {category:cls.ibank_balance}
             return cls.ibank_balance
         elif bank == 'pbank':
             if stat == 'income':
                 cls.pbank_balance += amount
+                cls.pbank_list[stat] = {category:cls.pbank_balance}
             else:
                 cls.pbank_balance -= amount
-            cls.pbank_list.append(cls.pbank_balance)
-            cls.category_list.append(category)
+                cls.pbank_list[stat] = {category:cls.pbank_balance}
             return cls.pbank_balance
     
     @classmethod
@@ -44,7 +40,7 @@ class Account:
         return cls.total_balance
 
     def __str__(cls):
-        return f'Total balance: {Account.total_Balance()}\n hbank: {cls.hbank_list}\n ibank: {cls.ibank_list}\n pbank: {cls.pbank_list}\n category: {cls.category_list}'
+        return f'Total balance: {Account.total_Balance()}\n hbank: {cls.hbank_list}\n ibank: {cls.ibank_list}\n pbank: {cls.pbank_list}'#\n category: {cls.category_list}'
         
 print(Account.update_balance('hbank',1000,'income','sal'))
 print(Account.update_balance('ibank',1000,'income','transfer'))

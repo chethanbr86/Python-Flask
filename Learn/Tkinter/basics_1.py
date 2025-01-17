5809ce3c0603d1721e88ed1236682030ee9f6153
Chethan BR <chethanbr86@gmail.com>
2025-01-17 20:57:24 +0530

    tk v2

--- Learn/Tkinter/basics_1.py
+++ Learn/Tkinter/basics_1.py
@@ -4,16 +4,17 @@ import ttkbootstrap as ttkb
 
 def convert():
     return output_string.set(entry_int.get()*1.61)
+    # return output_string.config(entry_int.get()*1.61) 
 
 #window
 # window = tk.Tk()
 window = ttkb.Window(themename='journal') #darkly
-window.title('Test')
+window.title('Convertor')
 window.geometry('800x300')
 
 #title
 title_label = ttkb.Label(master=window, text='Miles to KM', font='Ariel 20 bold')
-title_label.pack()
+title_label.pack() #title_label.grid(row=0, column=0) #you can use grid instead of pack
 
 #input
 input_frame = ttkb.Frame(master=window)
